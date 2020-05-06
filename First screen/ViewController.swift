e2f84c4757054f963a8e78cba73950168155069c
Kirill <kirill_kornew@mail.ru>
2020-05-06 19:27:08 +0300

    added landscape orientation and gitignore

--- First screen/ViewController.swift
+++ First screen/ViewController.swift
@@ -14,14 +14,21 @@ class ViewController: UIViewController {
     let image = UIImage(named: "image")
     let imageView = UIImageView(image: image)
     imageView.translatesAutoresizingMaskIntoConstraints = false
+    imageView.contentMode = .scaleAspectFit
     return imageView
   }()
   
   private let descriptionTextView: UITextView = {
     let textView = UITextView()
-    textView.text = "Добро пожаловать в наше приложение"
+    
+    let attributedText = NSMutableAttributedString(string: "Добро пожаловать в наше приложение",
+                                                   attributes: [NSMutableAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 18)])
+    
+    attributedText.append(NSAttributedString(string: "\n\n\n Вы готовы к работе с приложением?",
+                                             attributes: [NSMutableAttributedString.Key.font : UIFont.systemFont(ofSize: 13),
+                                                          NSAttributedString.Key.foregroundColor : UIColor.gray]))
+    textView.attributedText = attributedText
     textView.translatesAutoresizingMaskIntoConstraints = false
-    textView.font = UIFont.boldSystemFont(ofSize: 18)
     textView.textAlignment = .center
     textView.isEditable = false
     textView.isSelectable = false
@@ -30,23 +37,26 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.addSubview(myImageView)
     view.addSubview(descriptionTextView)
     setupLayout()
   }
   
   private func setupLayout() {
-    //image
-    myImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    myImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-    myImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-    myImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-    //description
-    descriptionTextView.topAnchor.constraint(equalTo: myImageView.bottomAnchor, constant: 100).isActive = true
-    descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-    descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    let topImageContainerView = UIView()
+    view.addSubview(topImageContainerView)
+    topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+    topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+    topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    topImageContainerView.addSubview(myImageView)
+    myImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+    myImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+    myImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
+    topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+    descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
+    descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 25).isActive = true
+    descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -25).isActive = true
     descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
-    
   }
   
 
