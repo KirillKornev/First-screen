a5f9ccfa6989a09e392c844e88716f1dfada54ae
Kirill <kirill_kornew@mail.ru>
2020-05-07 16:25:41 +0300

    add folder

---
+++ First screen/UIColor+extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+extension.swift
+//  First screen
+//
+//  Created by Кирилл on 5/7/20.
+//  Copyright © 2020 Kornev.com. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  static var mainPink = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
+}
